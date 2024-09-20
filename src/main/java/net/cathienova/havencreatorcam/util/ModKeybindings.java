56447dfe1ec021b9243d1b4c06aa41cf2ee78aae
Cathienna <Cathienna69@gmail.com>
2024-09-20 17:58:46 +0200

    Added Back View Camera and fixed up code

--- src/main/java/net/cathienova/havencreatorcam/util/ModKeybindings.java
+++ src/main/java/net/cathienova/havencreatorcam/util/ModKeybindings.java
@@ -12,10 +12,17 @@ public class ModKeybindings
 
     private static final String CATEGORY = "key.categories.havencreatorcam";
 
-    public final KeyMapping creatorcam = new KeyMapping(
-            "key.havencreatorcam.havencreatorcam",
+    public final KeyMapping frontcam = new KeyMapping(
+            "key.havencreatorcam.havenfrontcam",
             KeyConflictContext.IN_GAME,
             InputConstants.getKey(InputConstants.KEY_Z, -1),
+            CATEGORY
+    );
+
+    public final KeyMapping backcam = new KeyMapping(
+            "key.havencreatorcam.havenbackcam",
+            KeyConflictContext.IN_GAME,
+            InputConstants.getKey(InputConstants.KEY_X, -1),
             CATEGORY
     );
 }
