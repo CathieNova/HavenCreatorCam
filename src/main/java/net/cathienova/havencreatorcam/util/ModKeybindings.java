46b8b863128813db2374b81f0c03e8cdffc40c9b
Cathienna <Cathienna69@gmail.com>
2024-09-20 17:29:01 +0200

    Added Back View Camera and fixed up code

--- src/main/java/net/cathienova/havencreatorcam/util/ModKeybindings.java
+++ src/main/java/net/cathienova/havencreatorcam/util/ModKeybindings.java
@@ -14,15 +14,24 @@ public class ModKeybindings
 
     private static final String CATEGORY = "key.categories." + HavenCreatorCam.MOD_ID;
 
-    public static final KeyMapping creatorCamKey = new KeyMapping(
-            "key." + HavenCreatorCam.MOD_ID + ".creatorCam",
+    public static final KeyMapping frontcam = new KeyMapping(
+            "key." + HavenCreatorCam.MOD_ID + ".havenfrontcam",
             KeyConflictContext.IN_GAME,
             InputConstants.Type.KEYSYM,
             InputConstants.KEY_Z,
             CATEGORY
     );
 
+    public static final KeyMapping backcam = new KeyMapping(
+            "key." + HavenCreatorCam.MOD_ID + ".havenbackcam",
+            KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM,
+            InputConstants.KEY_X,
+            CATEGORY
+    );
+
     public static void registerKeybindings() {
-        ClientRegistry.registerKeyBinding(creatorCamKey);
+        ClientRegistry.registerKeyBinding(frontcam);
+        ClientRegistry.registerKeyBinding(backcam);
     }
 }
